87b66b093bd2f8ab510dcc685c2b4f077bfb39c1
LakeFox <mason@lakefox.net>
2018-01-02 10:19:06 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -5,15 +5,26 @@ const base32 = require('base32');
 // /geokey/lat,long
 // /geokey/?lat=38.518093&lng=-93.529903
 app.get("/geokey/*", (req,res) => {
-  var lat = parseInt(req.query.lat);
-  var lng = parseInt(req.query.lng);
-  var latKey = (Math.floor((lat+90)*7200)).toString(16).toUpperCase();
-  var rawLng = Math.floor(Math.abs(lng)*7200);
-  var lngKey = rawLng.toString(16).toUpperCase();
-  if (lng < 0) {
-    lngKey = lngKey.toLowerCase();
+  if (req.query.lat && req.query.lng) {
+    var lat = parseFloat(req.query.lat);
+    var lng = parseFloat(req.query.lng);
+    var latKey = (Math.floor((lat+90)*7200)).toString(16).toUpperCase();
+    var rawLng = Math.floor(Math.abs(lng)*7200);
+    var lngKey = rawLng.toString(16).toUpperCase();
+    if (lng < 0) {
+      lngKey = lngKey.toLowerCase();
+    }
+    res.end(latKey+":"+lngKey);
+  } else if (req.query.key) {
+    var keys = req.query.key.split(":");
+    var lat = (parseInt(keys[0], 16)/7200)-90;
+    var n = 1;
+    if (keys[1].toUpperCase() != keys[1]) {
+      n = -1;
+    }
+    var lng = (parseInt(keys[1], 16)/7200)*n;
+    res.end(JSON.stringify({"lat": lat, "lng": lng}));
   }
-  res.end(latKey+":"+lngKey)
 });
 
 http.listen(8080, ()=>{
